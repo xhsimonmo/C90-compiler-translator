a4773d4d5e68a3fd5455243f8e846573e97f3f4a
Mo Shaoxin <nzsimonmo@gmail.com>
2020-03-02 13:18:40 +0000

    temporary removal of enum and struct and union in parser

---
+++ include/declarator.hpp
@@ -0,0 +1,31 @@
+#ifndef declarator_h
+#define declarator_h
+#include "ast.hpp"
+
+// declarator
+// 	: pointer direct_declarator  {$$ = new declarator($1, $2);}
+// 	| direct_declarator    {$$ = $1}
+// 	;
+
+class declarator : public astnode{
+public:
+  declarator(treeptr _ptr, treeptr _direct_decla):ptr(_ptr), direct_decla(_direct_decla){}
+  ~declarator(){
+    delete ptr;
+    delete direct_decla;
+  }
+private:
+  treeptr ptr == NULL;
+  treeptr direct_decla == NULL;
+}；
+
+void declarator :: translate(string& pyout){
+  
+}
+
+
+
+
+
+
+#endif
--- include/specifier_qualifier_list.hpp
+++ include/specifier_qualifier_list.hpp
@@ -2,7 +2,25 @@
 #define specifier_qualifier_list_h
 #include "ast.hpp"
 
-class
+// specifier_qualifier_list
+// 	: type_specifier specifier_qualifier_list {$$ = new specifier_qualifier_list($1,$2);}
+// 	| type_specifier  {$$ = $1}
 
+class specifier_qualifier_list : public astnode{
+public:
+  specifier_qualifier_list(treeptr _spec, treeptr _list):spec(_spec),list(_list){};
+  ~specifier_qualifier_list(){
+    delete spec;
+    delete list;
+  }
+private:
+  treeptr spec = NULL;
+  treeptr list = NULL;
+};
+
+void specifier_qualifier_list::translate(string& pyout)
+{
+  //skip, no type for python
+}
 
 #endif
