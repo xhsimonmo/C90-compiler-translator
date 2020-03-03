2373ef2ce03507632424a3dbd9e54a04602b0938
Mo Shaoxin <nzsimonmo@gmail.com>
2020-03-03 16:17:43 +0000

    c_compiler.cpp ahhhhhhhhhhhh

--- makefile
+++ makefile
@@ -1,7 +1,30 @@
 CPPFLAGS += -std=c++11 -W -Wall -g -Wno-unused-parameter
 CPPFLAGS += -I include
+#CPPFLAGS = -W -Wall
 
-//bin/c_compiler --translate [source-file.c] -o [dest-file.py]
+# invoke by bin/c_compiler --translate [source-file.c] -o [dest-file.py]
 
 
-bin/c_compiler
+bin/c_compiler : src/c_compiler.o src/compiler.tab.o src/lexer.yy.o src/compiler.tab.o  #format reference from Lab2
+	mkdir -p bin
+	g++ $(CPPFLAGS) -o bin/c_compiler $^
+
+src/c_compiler.o : src/c_compiler.cpp
+	g++ $(CPPFLAGS) -o src/c_compiler.o $^
+
+
+
+src/compiler.tab.cpp src/compiler.tab.hpp : src/compiler.y
+	bison -v -d src/compiler.y -o src/compiler.tab.cpp
+
+src/lexer.yy.cpp : src/lexer.flex src/compiler.tab.hpp
+	flex -o src/lexer.yy.cpp  src/lexer.flex
+
+
+
+
+clean :
+		rm src/*.o
+		rm bin/*
+		rm src/*.tab.cpp
+		rm src/*.yy.cpp
--- src/c_compiler.cpp
+++ src/c_compiler.cpp
@@ -1,30 +1,30 @@
-
-#include "compiler.y"
-#include "lexer.flex"
+//#include "compiler.y"
+//#include "lexer.flex"
 #include"ast.hpp"
 
 ///include every hpp file
-
-
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
 
 using namespace std;
-
 extern indentation;//specify number of tab/"/t"
-
 typedef std::shared_ptr<astnode> treeptr;
+//invoke by :
+//bin/c_compiler -S [source-file.c] -o [dest-file.s]
 
-int main(argc, argv[]){
+int main(int argc, argv*[]){
+  if(argc < 5)
   ifstream csource;//input ...c
   csourse.open(argv[0]);//open input c, tokenise it, lex, and parse.
-  
+
   std::cerr << "translator begin" << '\n';
   treeptr root;
   string pyout;
   root-> translate(pyout);
   string addmain;
-  addmain = "if __name__ == \"__main__\":\nimport sys\nret=main()\nsys.exit(ret)" // include in every python file to invoke main
+  addmain = "\nif __name__ == \"__main__\":\nimport sys\nret=main()\nsys.exit(ret)" // include in every python file to invoke main
   pyout = pyout + addmain;
   ofstream pyfile;//output ...py
   pyfile.open(argv[1]);
