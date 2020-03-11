a845b6b33f430f91690959d0b1246b73988d2f10
Mo Shaoxin <nzsimonmo@gmail.com>
2020-03-11 12:10:13 +0000

    lets get translator work today!

--- src/c_compiler.cpp
+++ src/c_compiler.cpp
@@ -32,14 +32,15 @@ int main(int argc, char *argv[]){
     int indentation = 0;
     std::string pyout;
     std::cout << "enter ast tree" << '\n';
-    std::cerr << "first root : "  << root -> testpoint() << '\n';
     root->translate(pyout, {});
-    
+    std::cerr << "//////////////end of translation//////////////" << std::endl;
     std::string addmain;
     addmain = "\nif __name__ == \"__main__\":\n\timport sys\n\tret=main()\n\tsys.exit(ret)"; // include in every python file to invoke main
-    std::cout << addmain << '\n';
+
     //pyout = pyout + addmain;
     pyout.append(addmain);
+    std::cerr << "the output python is :" << '\n';
+    std::cerr << pyout << '\n';
     pyfile << pyout;
 
   }
