c36f21f47e23e91a1f99d77db5f0f288d54d6ccc
Mo Shaoxin <nzsimonmo@gmail.com>
2020-03-27 09:33:08 +0000

    parameter kinda work

--- result.s
+++ result.s
@@ -1,58 +1,45 @@
 	.set noreorder
 	.text
 	.align 2
-	.globl main
-main:
+	.globl f
+f:
 addiu $29,$29,-12
 sw $30,4($29)
 sw $31,8($29)
 move $30,$29
 addiu $29,$29,-24
-addiu $29,$29,-168
+addiu $29,$29,-104
 nop
-#result_offset in init_declarator: 0
-#get a primary expression as number : 2
-li $2,2
-sw $2,-8($30)
-sw $2,0($30)
-#result_offset in init_declarator: -8
-#get a primary expression as number : 0
-li $2,0
+sw $4,12($30)
+sw $5,16($30)
+nop
+lw $2,12($30)
+nop
+sw $2,-12($30)
+lw $2,16($30)
+nop
 sw $2,-16($30)
-sw $2,-8($30)
-lw $2,0($30)
-nop
-sw $2,-20($30)
-lw $2,0($30)
-nop
-sw $2,-24($30)
-lw $2,-8($30)
-nop
-sw $2,-28($30)
 #load right index from memory
-lw $3,-28($30)
+lw $3,-16($30)
 nop
 #load left index from memory
-lw $2,-24($30)
+lw $2,-12($30)
 nop
-or $2,$2,$3
-sw $2,-32($30)
-nop
-sw $2,0($30)
+#additive expression
+add $2,$2,$3
+sw $2,-20($30)
 #get a primary expression as number : 2
 li $2,2
-sw $2,-36($30)
-lw $2,0($30)
+sw $2,-24($30)
+#load right index from memory
+lw $3,-24($30)
 nop
-sw $2,-40($30)
-lw $2,-36($30)
+#load left index from memory
+lw $2,-20($30)
 nop
-lw $3,-40($30)
-nop
-nop
-mult $2,$3
-mflo $2
-sw $2,-44($30)
+#additive expression
+add $2,$2,$3
+sw $2,-28($30)
 #////////ending current frame//////
 move $29,$30
 lw $31,8($29)
